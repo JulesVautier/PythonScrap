cac09c4fe4958c6546a92fac3f050097ec94128e
jules vautier <jules.vautier@epitech.eu>
2018-11-08 22:44:27 +0100

    clean

--- app/scraper.py
+++ app/scraper.py
@@ -4,21 +4,23 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 JSON_URL = 'https://s3-eu-west-1.amazonaws.com/legalstart/thumbscraper_input_tampered.hiring-env.json'
-FIRST_URL = 'https://yolaw-tokeep-hiring-env.herokuapp.com/'
+ROOT_URL = 'https://yolaw-tokeep-hiring-env.herokuapp.com/'
 
 
 class Scraper:
 
-    def __init__(self, url = JSON_URL):
-        self.url = url
+    def __init__(self, json_url=JSON_URL, root_url=ROOT_URL):
+        self.json_url = json_url
+        self.root_url = root_url
         pass
 
     def getJSON(self):
-        resp = requests.get(url=self.url)
+        resp = requests.get(url=self.json_url)
         data = resp.json()
         return data
 
     def readPage(self, pageURL, jsonURL, data, nbPage):
+        print('Move to page', nbPage)
         page = data[jsonURL]
         req = requests.get(url=pageURL, auth=HTTPBasicAuth('Thumb', 'Scraper'))
         root = lxml.html.fromstring(req.content)
@@ -27,11 +29,12 @@ class Scraper:
         query = root.xpath(page['xpath_test_query'])
         result = page['xpath_test_result']
         if (query == result):
-            print('Move to page', nbPage)
-            self.readPage(FIRST_URL + button, page['next_page_expected'], data, nbPage + 1)
+            self.readPage(self.root_url + button, page['next_page_expected'], data, nbPage + 1)
         else:
             print('ALERT - Can’t move to page ', nbPage + 1, ': page ', nbPage,' link has been malevolently tampered with!!', sep='')
+            pass
 
     def start(self):
         data = self.getJSON()
-        self.readPage(FIRST_URL, '0', data, 0)
+        self.readPage(self.root_url, '0', data, 1)
+        pass
